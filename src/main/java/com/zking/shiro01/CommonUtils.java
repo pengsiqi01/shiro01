318d2d2847b7fc97ec2ca72b51cdc5e96d1ee152
pengsiqi01 <1325129904@qq.com>
2020-12-18 10:43:27 +0800

    ok

--- src/main/java/com/zking/shiro01/CommonUtils.java
+++ src/main/java/com/zking/shiro01/CommonUtils.java
@@ -3,5 +3,7 @@ package com.zking.shiro01;
 public class CommonUtils {
     public static void main(String[] args) {
         System.out.println(System.currentTimeMillis());
+        System.out.println("我不知道");
     }
+
 }
